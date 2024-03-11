09a6be9939ee4833dc3fd044944b1420a955ccf3
Pusech <iinuzukakiba@gmail.com>
2024-03-11 22:40:14 +0100

    v1

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,30 @@
 import arrow from 'src/images/arrow.svg';
-
+import clsx from 'clsx';
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type OnClick = {
+	onClick: () => void;
+	isOpened: boolean;
+};
 
-export const ArrowButton = () => {
+export const ArrowButton = ({ onClick, isOpened }: OnClick) => {
+	const buttonClass = clsx(styles.container, {
+		[styles.container_open]: isOpened,
+	});
+	const arrowClass = clsx(styles.arrow, {
+		[styles.arrow_open]: isOpened,
+	});
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={buttonClass}
+			onClick={onClick}>
+			<img src={arrow} alt='иконка стрелочки' className={arrowClass} />
 		</div>
 	);
 };
